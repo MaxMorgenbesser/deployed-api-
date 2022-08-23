7db10224e65968867fc25b9a4dd73ce847b9ab41
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-08-22 22:02:26 -0400

    added emulators

--- functions/src/dogs.js
+++ functions/src/dogs.js
@@ -1,9 +1,10 @@
 export function getDogs(req,res){
     res.send([{name:'ryder', age:'22', gender:'MN'},
-{name:'duke', age:3, gender:"M"}])
+{name:'duke', age:3, gender:"M"},
+{name:'indigo', age:3, gender:"M"}
+])
 }
 
 export const testApi=(req,res)=>{
     res.send([{message:"true"}])
-
 }
